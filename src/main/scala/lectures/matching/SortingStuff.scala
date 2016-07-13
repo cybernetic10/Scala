d7de5add022bf03718df43ba3b9c053026a7ee46
Yablonskiy Sergey <fchaarlem@mail.ru>
2016-07-14 00:25:55 +0300

    SortingStuff - commented
    Комментарии и косметика

--- src/main/scala/lectures/matching/SortingStuff.scala
+++ src/main/scala/lectures/matching/SortingStuff.scala
@@ -30,78 +30,86 @@ package lectures.matching
 
 object SortingStuff extends App {
 
-  object Knife extends Stuff
+	object Knife extends Stuff
 
-  trait Stuff
+	trait Stuff
 
-  case class Boots(brand: String, size: Int = 43) extends Stuff
+	case class Boots(brand: String, size: Int = 43) extends Stuff
 
-  case class Watches(brand: String, cost: Float) extends Stuff
+	case class Watches(brand: String, cost: Float) extends Stuff
 
-  case class Book(name: String, isInteresting: Boolean = false) extends Stuff
+	case class Book(name: String, isInteresting: Boolean = false) extends Stuff
 
-  val stuff = List(
-    Boots("Adidas", 42),
-    Book("Game of thrones 1", true),
-    Boots("Reebok", 42),
-    Watches("Nautica", 10000),
-    Boots("Converse", 43),
-    Boots("Converse", 43),
-    Watches("Breitling", 100000),
-    Watches("Electronika", 1000),
-    Boots("Skorohod", 50),
-    Boots("Noname", 45),
-    Watches("Zarya", 15000),
-    Book("Game of thrones 3"),
-    Book("Game of thrones 4"),
-    Watches("Casio", 5000),
-    Watches("Citizen", 5000),
-    Book("Game of thrones 2", true),
-    Book("Idiot", true),
-    Book("Lubovnaya lubov"),
-    Knife
-  )
+	val stuff = List(
+		Boots("Adidas", 42),
+		Book("Game of thrones 1", true),
+		Boots("Reebok", 42),
+		Watches("Nautica", 10000),
+		Boots("Converse", 43),
+		Boots("Converse", 43),
+		Watches("Breitling", 100000),
+		Watches("Electronika", 1000),
+		Boots("Skorohod", 50),
+		Boots("Noname", 45),
+		Watches("Zarya", 15000),
+		Book("Game of thrones 3"),
+		Book("Game of thrones 4"),
+		Watches("Casio", 5000),
+		Watches("Citizen", 5000),
+		Book("Game of thrones 2", true),
+		Book("Idiot", true),
+		Book("Lubovnaya lubov"),
+		Knife
+	)
 
-  case class StuffBox(books: List[Book] = Nil,
-                      watches: List[Watches] = Nil,
-                      boots: List[Boots] = Nil,
-                      junk: List[Stuff] = Nil)
+	case class StuffBox(books: List[Book] = Nil,
+	                    watches: List[Watches] = Nil,
+	                    boots: List[Boots] = Nil,
+	                    junk: List[Stuff] = Nil)
 
-  def sortJunk(stuff: List[Stuff]) = sort(stuff, StuffBox())
+	// Метод сортивки вещей, возвращает наполненную коробку
+	def sortJunk(stuff: List[Stuff]) = sort(stuff, StuffBox())
 
-  // Замените знаки вопроса подходящим кодом
-  // Поправьте логику метода
-  private def sort(stuff: List[Stuff], stuffBox: StuffBox): StuffBox = {
-    stuff match {
-      case head :: Nil => putStuffInRightBox(head, stuffBox)
-      case head :: tail =>
-        val newBox = putStuffInRightBox(head, stuffBox)
-        sort(tail, newBox)
-    }
-  }
+	// Замените знаки вопроса подходящим кодом
+	// Поправьте логику метода
+	private def sort(stuff: List[Stuff], stuffBox: StuffBox): StuffBox = {
+		stuff match {
+			// Если остался один элемент, то кладем его в коробку и возвращаем её
+			case head :: Nil => putStuffInRightBox(head, stuffBox)
+			// Если есть хвост, то кладем первый элемент в коробку, а для хвоста снова вызываем метод сортировки
+			case head :: tail =>
+				val newBox = putStuffInRightBox(head, stuffBox)
+				sort(tail, newBox)
+		}
+	}
 
-  // Метод должен положть вещь в правильную коробку
-  private def putStuffInRightBox(item: Stuff, stuffBox: StuffBox) = {
-    item match {
-      // Кладем обувь
-      case it: Boots if (it.brand == "Adidas" || it.brand == "Converse") => stuffBox.copy(boots = it :: stuffBox.boots)
-      // Кладем часы
-      case it: Watches if it.cost > 1000 => stuffBox.copy(watches = it :: stuffBox.watches)
-      // Кладем книги
-      case it@Book(_, true) => stuffBox.copy(books = it :: stuffBox.books)
-      // Кладем все остальное
-      case junk@_ => stuffBox.copy(junk = junk :: stuffBox.junk)
-    }
-  }
+	// Метод должен положить вещь в правильную коробку
+	private def putStuffInRightBox(item: Stuff, stuffBox: StuffBox) = {
+		item match {
+			// Кладем обувь
+			case it: Boots if (it.brand == "Adidas" || it.brand == "Converse") =>
+				stuffBox.copy(boots = it :: stuffBox.boots)
+			// Кладем часы
+			case it: Watches if it.cost > 1000 =>
+				stuffBox.copy(watches = it :: stuffBox.watches)
+			// Кладем книги
+			case it@Book(_, true) =>
+				stuffBox.copy(books = it :: stuffBox.books)
+			// Кладем все остальное
+			case junk@_ =>
+				stuffBox.copy(junk = junk :: stuffBox.junk)
+		}
+	}
 
-  def findMyKnife(stuffBox: StuffBox): Boolean =
-    stuffBox match {
-      case junk@stuffBox.junk if junk.contains(Knife) => true
-      case _ => false
-    }
+	// Поиск ножа
+	def findMyKnife(stuffBox: StuffBox): Boolean =
+		stuffBox match {
+			case junk@stuffBox.junk if junk.contains(Knife) => true
+			case _ => false
+		}
 
-  //вместо вопросов подставьте композицию функций  sortJunk и findMyKnife
-  val knifeIsInJunk = (sortJunk _ andThen findMyKnife) (stuff)
+	// Сортируем списко вещей и ищем в ней нож
+	val knifeIsInJunk = (sortJunk _ andThen findMyKnife) (stuff)
 
-  print(s"Is knife in a junk? - $knifeIsInJunk")
+	print(s"Is knife in a junk? - $knifeIsInJunk")
 }
